18fd3bf72ad62d2f1fcac7fd069bcaad9b0a9f07
Victor Hernandez-Urbina <hernandezurbina@vico.lan>
2018-08-26 07:25:28 +0100

    Added models and UX.

--- Vickea/ViewController.swift
+++ Vickea/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, ARSCNViewDelegate {
 
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    
+    @IBOutlet weak var planeDetectedLabel: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    let configuration = ARWorldTrackingConfiguration()
+    var selectedItem: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        
+        self.sceneView.session.run(configuration)
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
+        self.sceneView.delegate = self
+        self.sceneView.autoenablesDefaultLighting = true
+        
+        self.registerGestureRecognizers()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,121 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func registerGestureRecognizers(){
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(rotate))
+        longPressGestureRecognizer.minimumPressDuration = 0.1
+        
+        self.sceneView.addGestureRecognizer(pinchGestureRecognizer)
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.addGestureRecognizer(longPressGestureRecognizer)
+    }
+    
+    @objc func rotate(sender: UILongPressGestureRecognizer){
+        let sceneView = sender.view as! ARSCNView
+        let holdLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(holdLocation)
+        if !hitTest.isEmpty{
+            let result = hitTest.first!
+            if sender.state == .began{
+                let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 1)
+                result.node.runAction(SCNAction.repeatForever(action))
+            } else if sender.state == .ended{
+                result.node.removeAllActions()
+            }
+        }
+    }
+    
+    @objc func pinch(sender: UIPinchGestureRecognizer){
+        let sceneView = sender.view as! ARSCNView
+        
+        let pinchLocation = sender.location(in: sceneView)
+        
+        let hitTest = sceneView.hitTest(pinchLocation)
+        
+        if !hitTest.isEmpty{
+            let results = hitTest.first!
+            let node = results.node
+            let pinchAction = SCNAction.scale(by: sender.scale, duration: 0)
+            node.runAction(pinchAction)
+            sender.scale = 1.0
+        }
+    }
+    
+    @objc func tapped(sender: UITapGestureRecognizer){
+        
+        let sceneView = sender.view as! ARSCNView
+        
+        let tappedLocation = sender.location(in: sceneView)
+        
+        let hitTest = sceneView.hitTest(tappedLocation, types: .existingPlaneUsingExtent)
+        
+        if !hitTest.isEmpty{
+            self.addItem(hitTestResult: hitTest.first!)
+        } else{
+            print("No match")
+        }
+    }
+    func addItem(hitTestResult: ARHitTestResult){
+        if let selectedItem = self.selectedItem{
+            let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn")
+            let node = (scene?.rootNode.childNode(withName: selectedItem, recursively: false))!
+            let transform = hitTestResult.worldTransform
+            let thirdColumn = transform.columns.3
+            node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+            if selectedItem == "table"{
+                self.centerPivot(for: node)
+            }
+            self.sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! itemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        self.selectedItem = itemsArray[indexPath.row]
+        cell?.backgroundColor = UIColor.green
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.blue
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else{return}
+        
+        DispatchQueue.main.async {
+            self.planeDetectedLabel.isHidden = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.planeDetectedLabel.isHidden = true
+            }
+
+        }
+    }
+    
+    func centerPivot(for node: SCNNode) {
+        let min = node.boundingBox.min
+        let max = node.boundingBox.max
+        node.pivot = SCNMatrix4MakeTranslation(
+            min.x + (max.x - min.x)/2,
+            min.y + (max.y - min.y)/2,
+            min.z + (max.z - min.z)/2
+        )
+    }
+}
+
+extension Int{
+    var degreesToRadians: Double{ return Double(self) * .pi/180 }
 }
 
---
+++ Vickea/itemCell.swift
@@ -0,0 +1,14 @@
+//
+//  itemCell.swift
+//  Vickea
+//
+//  Created by Victor Hernandez-Urbina on 19/08/2018.
+//  Copyright © 2018 Herurbi. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
